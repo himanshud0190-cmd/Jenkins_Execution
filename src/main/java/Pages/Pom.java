a100a32b28e5d8dc1d280591e5738a60ee8845ea
Himanshu@Dalal <himanshuapachertr160@gmail.com>
2025-10-01 07:29:28 +0530

    Jenkins

--- src/main/java/Pages/Pom.java
+++ src/main/java/Pages/Pom.java
@@ -1,21 +1,13 @@
 package Pages;
 
-import java.beans.Visibility;
-import java.time.Duration;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 public class Pom 
 {
 	 WebDriver driver;
-	
 	@FindBy (id = "pushDenied") private WebElement clickingPopup;
 	@FindBy ( xpath = "(//img[@title='Snapdeal'])[1]") private WebElement verifyingLogo;
 	@FindBy(xpath = "//iframe[@id='loginIframe']") private WebElement Frame;
